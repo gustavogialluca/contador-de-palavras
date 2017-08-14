b652029665738e10ee67e5da640ccdbd436b2932
gustavogialluca <gustavogialluca@gmail.com>
2017-08-14 20:29:14 -0300

    main pronta

--- src/main.c
+++ src/main.c
@@ -1,22 +1,60 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+
+int num (char c){
+	if(c >= '0' && c <= '9'){
+		return 1;
+	}
+	else return 0;
+}
+
+int letra (char c){
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+		return 1;
+	}
+	else return 0;
+}
 
 int main() {
 
-  char c;
-
-  c = 1;
+	char c, prev = 0;
+	int n = 0, v = 0;
 
   while (c != '\n') {
-    scanf("%c", &c);
-  }
+	
+	scanf("%c", &c);
+	
+	if (!(letra(c)) && !(num(c))){
+		if (letra(prev)){
+			n++;
+		}
+		
+		if (num(prev)){
+			if (v == 0 && ((c != ',') || (c != '.'))){			
+				n++;			
+				v = 0;
+			}
+			
+			while (v == 1){
+				if ((num(c)) && ((prev == ',') || (prev == '.'))){
+					n++;			
+				}
+				
+				if ((num(c)) && (num(prev))){
+					n++;
+				}
+				else v = 0;
+			}	
+		}	
+	}	
 
-  printf("1\n");
-  return 0;
+	if ((num(prev)) && ((c == ',') || (c == '.')) && (v == 0)){
+		v = 1;	
+	}
+	
+	prev = c;
+	
+  }
+	
+  	printf ("%d\n", n);
+	return 0;
 }
